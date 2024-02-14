e795a408e4d0fb2358a01b3d8c8926e205a56248
Sergii <nezalonov.serhii@gmail.com>
2024-02-14 02:47:38 +0200

    fixed popularProductSlice

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slice/productSlice";
-import popularProductReducer from "./slice/productSlice/popularProductSlice";
+import popularProductReducer from "./slice/popularProductSlice";
 
 const store = configureStore({
    reducer: {
       product: productReducer,
       popularProducts: popularProductReducer,
-      // Добавьте другие срезы по мере необходимости
    },
 });
 
